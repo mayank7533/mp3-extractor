11b63f25cf252d2a8d921fd6d380038d61ee1b75
mayank <mayank7533@outlook.com>
2017-11-23 12:56:32 +0530

    force close issue fixed

--- mp3 Extractor.py
+++ mp3 Extractor.py
@@ -12,10 +12,19 @@ clip = mp.VideoFileClip("myvideo.mp4").subclip(0,20)
 clip.audio.write_audiofile("theaudio.mp3")
 '''
 currentDir = os.getcwd()
+dirList = []
+for path,dirs,files in os.walk(currentDir):
+    for file in files:
+        dirList.append(str(file))
 for path,dirs,files in os.walk(currentDir):
     for file in files:
         nameArr = file.split('.')
         forma =  nameArr[len(nameArr)-1]
         if forma.lower() == "mkv" or forma.lower() == "mp4" or forma.lower() == "flv":
-            clip = mp.VideoFileClip(file)
-            clip.audio.write_audiofile(nameArr[0]+".mp3")
+            try:
+                clip = mp.VideoFileClip(file)
+                newName = nameArr[0]+".mp3"
+                if newName not in dirList:
+                    clip.audio.write_audiofile(nameArr[0]+".mp3")
+            except:
+                print("cannot convert ",file)
