bc4dbf9475de2bf1a8ed992d362de8b32a757c16
mayank <mayank7533@outlook.com>
2017-12-08 18:15:07 +0530

    added flv and webm

--- mp3 Extractor.py
+++ mp3 Extractor.py
@@ -3,7 +3,7 @@ import os.path
 imageio.plugins.ffmpeg.download()
 import moviepy.editor as mp
 import time
-
+from tkinter.filedialog import askdirectory
 currentDir = os.getcwd()
 dirList = []
 for path,dirs,files in os.walk(currentDir):
@@ -12,13 +12,13 @@ for path,dirs,files in os.walk(currentDir):
 for path,dirs,files in os.walk(currentDir):
     for file in files:
         nameArr = file.split('.')
-        forma =  nameArr[len(nameArr)-1]
-        if forma.lower() == "mkv" or forma.lower() == "mp4" or forma.lower() == "flv":
+        forma = nameArr[len(nameArr)-1]
+        if forma.lower() in ['mkv','mp4','flv','webm','avi']:
             try:
                 clip = mp.VideoFileClip(file)
                 newName = str(file)[:-4]+".mp3"
                 if newName not in dirList:
                     clip.audio.write_audiofile(newName)
             except:
-                print("cannot convert ",file)
+                print("cannot convert", file)
 time.sleep(5)
